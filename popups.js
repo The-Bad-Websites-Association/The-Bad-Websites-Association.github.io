5c616e58f7c451d20a0f72143d55976ae3d3b337
sudokufan <alexrichardgeorge@protonmail.com>
2019-10-01 12:56:33 +0100

    wrote first pass at a popup onload function/event

--- popups.js
+++ popups.js
@@ -1,2 +1,5 @@
-person.addEventListener('click', function(e){
-    alert(e.target.dataset.age)
+window.addEventListener('onload', function(e) {
+    alert('welcome to our website!')
+    alert('we\'re so excited to have you here!')
+    alert('feel free to take a look around!')
+}
