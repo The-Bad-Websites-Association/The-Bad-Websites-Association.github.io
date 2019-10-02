70d2e015762c8912a1aed43a08bd011d08cf09d4
Josh Selby <jdselby24@gmail.com>
2019-10-02 09:27:08 +0100

    Update popups.js

--- popups.js
+++ popups.js
@@ -1,4 +1,4 @@
-window.addEventListener('onload', function(e) {
+window.addEventListener('onload', function() {
     alert('welcome to our website!!!!!!!')
     alert('we\'re so excited to have you here!!!!!')
     alert('feel free to take a look around!!!!!!')
